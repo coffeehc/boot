bb993c4011629e4ce81729fdaed0b2d868c443e6
coffee <huangchuan@51zhangdan.com.cn>
2016-06-17 11:43:46 +0800

    全面改版 serviceboot 的 service 方式 1.增加了初始化方法 2.采用 yaml 配置文件的方式来处理配置 3.去除了 flags获取配置的方式,但有部分保留 4.service 启动失败不再重试,直接退出 5.增加了ErrorRecover方法,用于在 endpoint 方法开始时defer 已知的异常,并封装为统一的异常格式

---
+++ serviceboot/config.go
@@ -0,0 +1,51 @@
+package serviceboot
+
+import (
+	"flag"
+	"fmt"
+	"time"
+
+	"github.com/coffeehc/microserviceboot/base"
+	"github.com/coffeehc/web"
+)
+
+var configPath = flag.String("config", "", "配置文件路径")
+
+type ServiceConfig struct {
+	WebServerConfig *WebConfig   `yaml:"webserver"`
+	Debug           *DebugConfig `yaml:"debug"`
+}
+
+type WebConfig struct {
+	ServerAddr     string        `yaml:"serverAddr"`
+	ReadTimeout    time.Duration `yaml:"readTimeout"`
+	WriteTimeout   time.Duration `yaml:"eriteTimeout"`
+	MaxHeaderBytes int           `yaml:"maxHeaderBytes"`
+}
+
+func (this *ServiceConfig) getDebugConfig() *DebugConfig {
+	if this.Debug == nil {
+		this.Debug = &DebugConfig{}
+	}
+	return this.Debug
+}
+
+func (this *ServiceConfig) GetWebServerConfig() *web.ServerConfig {
+	webConfig := new(web.ServerConfig)
+	wc := this.WebServerConfig
+	if wc == nil {
+		wc = new(WebConfig)
+	}
+	if wc.ServerAddr == "" {
+		webConfig.ServerAddr = fmt.Sprintf("%s:8888", base.GetLocalIp())
+	} else {
+		webConfig.ServerAddr = wc.ServerAddr
+	}
+	//host,ip,err:=net.SplitHostPort(webConfig.ServerAddr)
+	//if host == ""
+	webConfig.ReadTimeout = wc.ReadTimeout
+	webConfig.WriteTimeout = wc.WriteTimeout
+	webConfig.MaxHeaderBytes = wc.MaxHeaderBytes
+	webConfig.DefaultTransport = web.Transport_Json
+	return webConfig
+}
