77c7f6aef2fbc9421b4b91a2ce08d7172ef71a5d
coffee <huangchuan@51zhangdan.com.cn>
2016-06-17 11:27:55 +0800

    1.增加了对 API 访问的异常信息的同一封装 2.增加了一个工具方法,用于拦截 panic 异常,打印后再将 panic 透传出去 3.增加了 init 方法的参数,使之在服务启动之前还可以根据配置做个性化变更,以及修改 web 容易参数 4.增加了一个获取指定长度的随机数方法

--- base/errors.go
+++ base/errors.go
@@ -7,6 +7,51 @@ import (
 	"github.com/coffeehc/web"
 )
 
+type BizErr struct {
+	httpCode  int64
+	debugCode int64
+	msg       string
+}
+
+func (this BizErr) Error() string {
+	return this.msg
+}
+
+func (this BizErr) GetHttpCode() int64 {
+	if this.httpCode == 0 {
+		return http.StatusBadRequest
+	}
+	return this.httpCode
+}
+
+func (this BizErr) GetDebugCode() int64 {
+	return this.debugCode
+}
+
+func (this BizErr) ToError() Error {
+	return Error{
+		Code:     int32(this.GetHttpCode()),
+		Debug_id: int64(this.GetDebugCode()),
+		Message:  this.Error(),
+	}
+}
+
+//默认第一个为 httpCode, 第二个为debugCode
+func BuildBizErr(err string, codes ...int64) BizErr {
+	var httpCode, debugCode int64
+	if len(codes) > 0 {
+		httpCode = codes[0]
+	}
+	if len(codes) > 1 {
+		debugCode = codes[1]
+	}
+	return BizErr{
+		msg:       err,
+		httpCode:  httpCode,
+		debugCode: debugCode,
+	}
+}
+
 type Error struct {
 	Code             int32  `json:"code"`
 	Debug_id         int64  `json:"debug_id"`
@@ -14,15 +59,26 @@ type Error struct {
 	Information_link string `json:"information_link"`
 }
 
-func NewSimpleError(code int32, message string) Error {
-	return Error{Code: code, Message: message}
+func ErrorToResponseError(err error) *Error {
+	if err == nil {
+		return nil
+	}
+	return NewSimpleError(-1, err.Error())
+}
+
+func (this Error) Error() string {
+	return fmt.Sprintf("%d:%s", this.Code, this.Message)
+}
+
+func NewSimpleError(code int32, message string) *Error {
+	return &Error{Code: code, Message: message}
 }
 
 type ErrorResponse struct {
-	Errors Error `json:"error"`
+	Errors *Error `json:"error"`
 }
 
-func NewErrorResponse(errs Error) ErrorResponse {
+func NewErrorResponse(errs *Error) ErrorResponse {
 	return ErrorResponse{errs}
 }
 
