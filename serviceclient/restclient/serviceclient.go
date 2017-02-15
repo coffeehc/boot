4fa951bfe619e71adccc81191381486df75c7c44
coffee <coffeehc@gmail.com>
2017-02-15 16:58:10 +0800

    变更ServiceClient接口

--- serviceclient/restclient/serviceclient.go
+++ serviceclient/restclient/serviceclient.go
@@ -11,11 +11,13 @@ import (
 	"github.com/coffeehc/microserviceboot/consultool"
 	"github.com/hashicorp/consul/api"
 	"github.com/coffeehc/microserviceboot/base/restbase"
-	"io/ioutil"
 )
 
 type ServiceClient interface {
+	GetServiceName() string
 	GetBaseUrl() string
+	GetHttpClient() client.HTTPClient
+	BuildRequest(endpintMeta restbase.EndpointMeta,query string) client.HTTPRequest
 }
 
 func NewServiceClient(serviceInfo base.ServiceInfo, httpClientConfig *client.HTTPClientOptions, discoveryConfig interface{}) (ServiceClient, base.Error) {
@@ -75,50 +77,12 @@ func (sc *_ServiceClient) GetHttpClient() client.HTTPClient {
 	return sc.client
 }
 
-func (sc *_ServiceClient)Do(endpintMeta restbase.EndpointMeta, requestData, responstData interface{}, contentType ContentType) base.Error {
-	if contentType == nil {
-		contentType = JsonContentType
-	}
+func (sc *_ServiceClient)BuildRequest(endpintMeta restbase.EndpointMeta,query string) client.HTTPRequest {
 	req := client.NewHTTPRequest()
 	method := string(endpintMeta.Method)
 	req.SetMethod(method)
-	uri := fmt.Sprintf("%s/%s", sc.baseURL, endpintMeta.Path)
-	if method != "POST" && method != "PUT" && method != "PATCH" && method != "OPTIONS" {
-		if query, ok := requestData.(string); ok {
-			uri = fmt.Sprintf("%s?%s", uri, query)
-		} else {
-			return base.NewError(base.ErrCodeBaseRPCInvalidArgument, "rest client", "requestData is not query")
-		}
-	} else {
-		if requestData != nil {
-			req.SetContentType(contentType.GetContenType())
-			data, err := contentType.Encode(requestData)
-			if err != nil {
-				return base.NewErrorWrapper("rest client", err)
-			}
-			req.SetBody(data)
-		}
-	}
-	req.SetURI(uri)
-	response,err:=sc.client.Do(req,false)
-	if err!=nil{
-		return base.NewErrorWrapper("rest client",err)
-	}
-	//response.GetContentType() == ""
-	body :=response.GetBody()
-	defer body.Close()
-	if response.SetStatusCode() != 200{
-		return base.NewError(base.ErrCodeBaseRPCAborted,"rest client",fmt.Sprintf("response code is ",response.SetStatusCode()))
-	}
-	data ,err:= ioutil.ReadAll(body)
-	if err!=nil{
-		return base.NewErrorWrapper("rest client", err)
-	}
-	err = contentType.Decoder(data,responstData)
-	if err!=nil{
-		return base.NewErrorWrapper("rest client", err)
-	}
-	return nil
+	req.SetURI(fmt.Sprintf("%s/%s?%s", sc.baseURL, endpintMeta.Path,query))
+	return req
 }
 
 
